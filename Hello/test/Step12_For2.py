569106f974fdeb2bcde59f62efc3b1e25f9249ba
autumnalkitty <nora.matamune@gmail.com>
2018-09-17 11:18:15 +0900

    python basic ended

---
+++ Hello/test/Step12_For2.py
@@ -0,0 +1,46 @@
+#-*- coding:utf-8 -*-
+
+'''
+
+'''
+names=['kim', 'lee', 'park', 'jo', 'choi']
+
+for item in names:
+    print item
+print '----------'
+
+for i in [0, 1, 2, 3, 4]:
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+for i in range(5):
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+for i in range(len(names)):
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+for i in [4, 3, 2, 1, 0]:
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+for i in range(4, -1, -1):
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+for i in range(len(names)-1, -1, -1):
+    tmp=names[i]
+    print u'{}: {}'.format(i, tmp)
+print '----------'
+
+#∑100
+sum100=0
+for item in range(1, 101):
+    sum100=sum100+item
+print sum100
