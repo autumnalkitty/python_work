abf2b7cd2b89f8f1a4d10bc6a7c5e8abe4d8d5d4
autumnalkitty <nora.matamune@gmail.com>
2018-09-18 11:31:34 +0900

    Step03_Extends added

---
+++ Hello/advance/Step03_Extends.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+
+class Phone():
+    def call(self):
+        print u"전화를 걸어요."
+
+class MobilePhone(Phone): #Phone 클래스를 상속받는 문법
+    def mobileCall(self):
+        print u"이동 중에 전화를 걸어요."
+    
+    def takePicture(self):
+        print u"100만 화소 사진을 찍어요."
+
+class SmartPhone(MobilePhone): #MobilePhone 클래스를 상속받는 문법
+    def doInternet(self):
+        print u"인터넷을 해요."
+        
+    def takePicture(self):
+        #부모 메소드 호출 여부 
+        #HandPhone.takePicture(self)
+        print u"1000만 화소 사진을 찍어요."
+        
+if __name__ == '__main__':
+    p1=Phone()
+    p2=MobilePhone()
+    p3=SmartPhone()
+    p3.call()
+    p3.mobileCall()
+    p3.doInternet()
+    p3.takePicture()
